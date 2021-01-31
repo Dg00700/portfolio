c22f2cfcf1ebc5d93b3d20ff0583b5bc8c0d51bb
Dg00700 <36060067+Dg00700@users.noreply.github.com>
2021-01-31 12:00:10 -0500

    Updates

---
+++ precache-manifest.4fcc487ee00c7c12cb0bfd122692cf4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1354e27d30f04bfdba8eb9aeb69925",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bf936515d3fb569bfb75",
+    "url": "/portfolio/static/css/main.3c4a3e1c.chunk.css"
+  },
+  {
+    "revision": "b8489bce6694240d6ccc",
+    "url": "/portfolio/static/js/2.54fbe9ff.chunk.js"
+  },
+  {
+    "revision": "fc5ece7cc949095cf090bb5905c5d2e6",
+    "url": "/portfolio/static/js/2.54fbe9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf936515d3fb569bfb75",
+    "url": "/portfolio/static/js/main.70c80566.chunk.js"
+  },
+  {
+    "revision": "309a9e55c61ba5bff665",
+    "url": "/portfolio/static/js/runtime-main.b3df5f95.js"
+  },
+  {
+    "revision": "71cc0ba80d62d29e8dce28dac716d229",
+    "url": "/portfolio/static/media/portfolio.71cc0ba8.png"
+  }
+]);
